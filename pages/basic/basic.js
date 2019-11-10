0c5c4b38050a736a96577b0c979f0d313a9c9779
DragonWang-cell <56779824+DragonWang-cell@users.noreply.github.com>
2019-11-10 23:35:54 +0800

    vvv
    vvv

---
+++ pages/basic/basic.js
@@ -0,0 +1,39 @@
+// pages/basic/basic.js
+Page({
+  jump1:function(){
+    wx.navigateTo({
+      url: './basic/basic1',
+    })
+  },
+  jump2: function () {
+    wx.navigateTo({
+      url: './basic/basic2',
+    })
+  },
+  
+  jump3: function () {
+    wx.navigateTo({
+      url: './basic/basic3',
+    })
+  },
+  jump4: function () {
+    wx.navigateTo({
+      url: './basic/basic4',
+    })
+  },
+  jump5: function () {
+    wx.navigateTo({
+      url: './basic/basic5',
+    })
+  },
+  jump6: function () {
+    wx.navigateTo({
+      url: './basic/basic6',
+    })
+  },
+  jump7: function () {
+    wx.navigateTo({
+      url: './basic/basic7',
+    })
+  }
+})
